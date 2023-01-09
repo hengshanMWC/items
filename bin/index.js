af2dc909d70105bd3dff308a41041069719c82b4
mawencan <1819293933@qq.com>
2023-01-10 00:56:19 +0800

    chore: 基本逻辑，打包跟eslint有问题

---
+++ bin/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { program } from 'commander'
+import inquirer from 'inquirer'
+import fs from 'fs-extra'
+import {  templateList } from '../dist/items.js'
+import { resolve, dirname } from "path";
+import { fileURLToPath } from 'url';
+import { createTemplate } from './utils.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pkg = await fs.readJSON(resolve(__dirname, '../package.json'))
+program
+  .version(pkg.version)
+  .description('Create item Template')
+program
+  .command('create [name]')
+  .description('Create item Template')
+  .action(async (name) => {
+    const answer = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'create',
+        message: 'Please select a template',
+        choices: templateList.map(item => item.name),
+      },
+    ])
+    createTemplate(answer.create, name)
+  })
+program.parse(process.argv)
