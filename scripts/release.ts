444e823e5e613056f829c9974e7e4800e570331d
mawencan <1819293933@qq.com>
2023-01-10 12:19:29 +0800

    chore: publish为公开包

--- scripts/release.ts
+++ scripts/release.ts
@@ -4,7 +4,7 @@ import pkg from '../package.json'
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
   execSync('npm run build', { stdio: 'inherit' })
-  execSync('npm publish', { stdio: 'inherit' })
+  execSync('npm publish --access public', { stdio: 'inherit' })
   execSync(`git tag v${pkg.version}`, { stdio: 'inherit' })
 })()
 console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
