f72052368b9dba9dd0eab762ad803bfd14278d6b
mawencan <1819293933@qq.com>
2023-05-28 17:58:29 +0800

    chore: 发布命令修改

--- scripts/release.ts
+++ scripts/release.ts
@@ -10,16 +10,16 @@
 // console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
 import { execSync } from 'child_process'
 import colors from 'colors'
-import { versionBump } from 'bumpp'
-// import pkg from '../package.json'
+// import { versionBump } from 'bumpp'
+import pkg from '../package.json'
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
   // 为了防止每次子包没有更新到pnpm-lock，报frozen-lockfile
   execSync('pnpm i', { stdio: 'inherit' })
   // execSync('npm run test', { stdio: 'inherit' })
   execSync('npm run build', { stdio: 'inherit' })
-  await versionBump()
-  // execSync(`git tag v${pkg.version}`, { stdio: 'inherit' })
+  // await versionBump()
+  execSync(`git tag v${pkg.version}`, { stdio: 'inherit' })
   execSync('npm run publish', { stdio: 'inherit' })
   execSync('git push origin HEAD', { stdio: 'inherit' })
 })()
