b0909edaafe402f161eff9805bff6b6919daa788
mawencan <1819293933@qq.com>
2023-05-28 17:21:11 +0800

    feat: 把功能从bin搬到源码

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,5 @@
 #!/usr/bin/env node
-import { program } from 'commander'
-import inquirer from 'inquirer'
-import fs from 'fs-extra'
-import { templateList, createTemplate } from '../dist/items.js'
-import { resolve, dirname } from "path";
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const pkg = await fs.readJSON(resolve(__dirname, '../package.json'))
-program
-  .version(pkg.version)
-  .description('Create item Template')
-program
-  .command('create [name]')
-  .description('Create item Template')
-  .action(async (name) => {
-    const answer = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'create',
-        message: 'Please select a template',
-        choices: templateList.map(item => item.name),
-      },
-    ])
-    createTemplate(answer.create, name)
-  })
-program.parse(process.argv)
+const {
+  cliMain,
+} = require('../dist/items.min')
+cliMain(process.argv)
