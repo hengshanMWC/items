59f51db2814fc18ba707570b83dc06d4c2da913f
mawencan <1819293933@qq.com>
2023-06-01 22:35:25 +0800

    chore: 去掉pnpm i

--- scripts/release.ts
+++ scripts/release.ts
@@ -14,8 +14,6 @@ import colors from 'colors'
 import pkg from '../package.json'
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
-  // 为了防止每次子包没有更新到pnpm-lock，报frozen-lockfile
-  execSync('pnpm i', { stdio: 'inherit' })
   // execSync('npm run test', { stdio: 'inherit' })
   execSync('npm run build', { stdio: 'inherit' })
   // await versionBump()
