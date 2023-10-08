57105538d9ffc9790f773f33d85495b37d0f2e3e
mawencan <1819293933@qq.com>
2023-10-08 15:40:10 +0800

    chore(package.json): 使用commit做提交,使用pkgs做版本维护

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,24 +1,10 @@
-// import { execSync } from 'child_process'
-// import colors from 'colors'
-// import { versionBump } from 'bumpp'
-// console.log(`${colors.cyan.bold('release: start')} 🏗`);
-// (async function () {
-//   execSync('npm run build', { stdio: 'inherit' })
-//   await versionBump()
-//   execSync('npm publish --access public', { stdio: 'inherit' })
-// })()
-// console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
 import { execSync } from 'child_process'
 import colors from 'colors'
-// import { versionBump } from 'bumpp'
-import pkg from '../package.json'
+import { commandPublish, commandVersion } from '@abmao/pkgs'
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
-  // execSync('npm run test', { stdio: 'inherit' })
   execSync('npm run build', { stdio: 'inherit' })
-  // await versionBump()
-  execSync(`git tag v${pkg.version}`, { stdio: 'inherit' })
-  execSync('npm publish --access public', { stdio: 'inherit' })
-  execSync('git push origin HEAD', { stdio: 'inherit' })
+  await commandVersion()
+  await commandPublish()
 })()
 console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
